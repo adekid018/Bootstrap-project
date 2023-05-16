34fe26fe8fe4fb27f4a418a98ff559a1319782f2
unknown <adebayoaa519@gmail.com>
2023-05-16 01:02:25 +0100

    adjusted footer and change images and other thing in online payment
    made the coursel button work in a convinent way

--- bootstrap project 8/online_payment.js
+++ bootstrap project 8/online_payment.js
@@ -7,10 +7,11 @@ let coursel_btn_2=document.querySelector(".coursel_btn_2")
 coursel_btn_right.addEventListener('click',()=>{
     coursel_item.scrollLeft += 80
     const screenWidth=coursel_item.scrollLeft
-    console.log(screenWidth);
+    console.log(`this is ${screenWidth}`);
+    console.log(`this is d body size ${window.innerWidth}`);
     if(screenWidth>=1030){
         coursel_btn_right.classList.add('disable')
-        console.log("yes");
+        //console.log("yes greater than 1030");
         coursel_btn_right.setAttribute("disabled","true")
     }
     //console.log(coursel_item.scrollWidth);
@@ -21,18 +22,26 @@ coursel_btn_left.addEventListener('click',()=>{
 })
 coursel_btn_1.addEventListener('click',()=>{
     coursel_item.innerHTML=coursel_item_array[0]
-    coursel_btn_1.style.backgroundColor="hsl(240, 76.2%, 8.2%)"
+    coursel_btn_1.classList.add("active")
+    coursel_btn_2.classList.remove("active")
+    coursel_btn_2.classList.add("inActive")
+    coursel_btn_1.classList.remove("inActive")
+    /*coursel_btn_1.style.backgroundColor="hsl(240, 76.2%, 8.2%)"
     coursel_btn_2.style.backgroundColor="rgba(158, 154, 154,0.3)"
     coursel_btn_1.style.color="white"
     coursel_btn_2.style.color="hsl(240, 76.2%, 8.2%)"    
-    console.log("hello");
+    console.log("hello");*/
 })
 
 coursel_btn_2.addEventListener('click',()=>{
-    coursel_btn_2.style.backgroundColor="hsl(240, 76.2%, 8.2%)"
+    /*coursel_btn_2.style.backgroundColor="hsl(240, 76.2%, 8.2%)"
     coursel_btn_2.style.color="white"
     coursel_btn_1.style.backgroundColor="rgba(158, 154, 154,0.3)"
-    coursel_btn_1.style.color="hsl(240, 76.2%, 8.2%)"
+    coursel_btn_1.style.color="hsl(240, 76.2%, 8.2%)"*/
+    coursel_btn_2.classList.add("active")
+    coursel_btn_1.classList.remove("active")
+    coursel_btn_1.classList.add("inActive")
+    coursel_btn_2.classList.remove("inActive")
     coursel_item.innerHTML=coursel_item_array[1]
     console.log("hello");
 })
